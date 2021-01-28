7d293910551e0f2d44df7bd099c866c2136e0084
mrdb12 <dbruchwald@gmail.com>
2021-01-28 16:51:32 +0100

    layout prettier and implementation of material.io

--- app/src/main/java/com/example/prototype_db/MainActivity.java
+++ app/src/main/java/com/example/prototype_db/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.prototype_db;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -16,18 +20,26 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView topicsRecView;
     private ArrayList<News> news = new ArrayList<>();
 
+    private View decorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
-        getSupportActionBar().hide(); //hide the title bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, //enables fullscreen
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if(visibility == 0){
+                    decorView.setSystemUiVisibility(hideSystemBars());
+                }
+            }
+        });
 
         topicsRecView = findViewById(R.id.topicsRecView);
 
-        news.add(new News("Das HLG während Corona", "abcde", "https://www.hlg-hamburg.de/wp-content/uploads/2014/08/IMG_6112.HLG_.jpg"));
+        news.add(new News("Das HLG während Corona","ababababababababababababababababababababababababababababababababababababababababababbababababababababababababababaababababababababa", "https://www.hlg-hamburg.de/wp-content/uploads/2014/08/IMG_6112.HLG_.jpg"));
         news.add(new News("Die Arbeit mit Moodel", "cdefg Moodel dies das", "https://i.ibb.co/F7d5cmc/moodle.jpg"));
         news.add(new News("UNESCO", "unesco schule auf ehre", "https://www.hlg-hamburg.de/wp-content/uploads/2014/08/DSCI02952-342x266.jpg"));
 
@@ -49,4 +61,20 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            decorView.setSystemUiVisibility(hideSystemBars());
+        }
+    }
+
+    public int hideSystemBars(){
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    }
 }
--- app/src/main/java/com/example/prototype_db/NewsActivity.java
+++ app/src/main/java/com/example/prototype_db/NewsActivity.java
@@ -3,6 +3,7 @@ package com.example.prototype_db;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -13,14 +14,22 @@ public class NewsActivity extends AppCompatActivity {
     private TextView newsText;
     private TextView newsHeadline;
 
+    private View decorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
-        getSupportActionBar().hide(); //hide the title bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, //enables fullscreen
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.news_layout);
+
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if(visibility == 0){
+                    decorView.setSystemUiVisibility(hideSystemBars());
+                }
+            }
+        });
 
         newsText = findViewById(R.id.newsText);
         newsHeadline = findViewById(R.id.newsHeadline);
@@ -32,5 +41,22 @@ public class NewsActivity extends AppCompatActivity {
         newsHeadline.setText(headline);
 
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            decorView.setSystemUiVisibility(hideSystemBars());
+        }
+    }
+
+    public int hideSystemBars(){
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
     }
 }
